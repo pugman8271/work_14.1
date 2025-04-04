7060d31fb52213b4e384d6e33c71c963b3c0ca0c
aslanhaag <aslanhaag@gmail.com>
2025-04-04 19:03:24 +0300

    add tests

---
+++ tests/test_classes.py
@@ -0,0 +1,14 @@
+def test_init_category(category_smart):
+    assert category_smart.name == "Смартфоны"
+
+
+def test_init_product(product_samsung):
+    assert product_samsung.name == "Samsung Galaxy S23 Ultra"
+
+
+def test_init_category_counter(category_smart):
+    assert category_smart.category_count == 1
+
+
+def test_init_product_counter(product_samsung):
+    assert product_samsung.product_count == 1
