a01cf683ea5d9d98f812779f5ba5a122a29f9386
aslanhaag <aslanhaag@gmail.com>
2025-04-20 18:17:14 +0300

    hot fixes

--- src/poduct_mixin.py
+++ src/poduct_mixin.py
@@ -8,4 +8,7 @@ class MixinProduct:
         print(repr(self))
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
+        return (f"\n"
+                f"**********\n"
+                f"Создан объект: {self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})\n"
+                f"**********\n")
--- tests/conftest.py
+++ tests/conftest.py
@@ -52,4 +52,7 @@ def lawn_grass():
 
 @pytest.fixture
 def prod_mixin_test():
-    return 'Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)'
+    return (f"\n"
+            f"**********\n"
+            f"Создан объект: Product(Samsung Galaxy S23 Ultra, 256GB, Серый цвет, 200MP камера, 180000.0, 5)\n"
+            f"**********\n")
