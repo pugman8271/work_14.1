5700ae493bc4e01c08ccd6f3952c3ecc068d289a
aslanhaag <aslanhaag@gmail.com>
2025-04-20 16:40:11 +0300

    fixes

--- src/poduct_mixin.py
+++ src/poduct_mixin.py
@@ -4,6 +4,8 @@ class MixinProduct:
     price: float
     quantity: int
 
-    def __init__(self, *args, **kwargs):
-        print(f"{self.__class__.__name__} ({self.name}, {self.description}, {self.price}, {self.quantity})")
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__} ({self.name}, {self.description}, {self.price}, {self.quantity})"
